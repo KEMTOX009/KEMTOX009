e1c1ffe824b218afdadac4cfc9c26e2c0646bbf1
KEMTOX009 <tomaszlabedz09@gmail.com>
2026-03-05 23:21:44 +0100

    First step

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Skrypt startowy
+console.log("Strona KEMTOX została załadowana!");
+
+// Przykład funkcji
+function przyklad() {
+    alert("Działa!");
+}
+
+// <button onclick="przyklad()">Kliknij</button>
 
