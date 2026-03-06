14f5b23372b93237ebdf371805dfd179fe8b17a9
KEMTOX009 <tomaszlabedz09@gmail.com>
2026-03-06 18:58:43 +0100

    update

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function spawnShape() {
 // generuj kształty co 200–500 ms
 function spawnLoop() {
     spawnShape();
-    setTimeout(spawnLoop, Math.random() * 300 + 200);
+    setTimeout(spawnLoop, Math.random() * 180 + 120);
 }
 
 spawnLoop();
