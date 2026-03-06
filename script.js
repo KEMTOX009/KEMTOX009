be58613287ffdcf6dd181ab57d662193913f26bd
KEMTOX009 <kemtox009@gmail.com>
2026-03-06 01:03:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.addEventListener("load", () => {
     const elements = document.querySelectorAll(".fade-in");
 
     elements.forEach((el, i) => {
-        el.style.animationDelay = `${3 + i * 0.25}s`; 
+        el.style.animationDelay = `${3 + i * 0.25}s`;
     });
 });
 
